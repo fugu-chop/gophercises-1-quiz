437a9d4a6535251abf5126277248c84fe0d1a9cb
Dean Wan <deanwan14@gmail.com>
2024-02-05 12:01:53 +1100

    feat: implement file open and data read

--- main.go
+++ main.go
@@ -1,5 +1,27 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"log"
+	"os"
+)
 
+// Need to read in a file (assume in the same director)
+// Defaults to problems.csv, but need to accept other options via flag(?)
+// Quizzes are short, so can read entire file into memory
+func main() {
+	// Open file
+	file, err := os.Open("./problems.csv")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	// Read data from file into memory
+	buffer := make([]byte, 2048)
+	rows, err := file.Read(buffer)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("read %d bytes: %q\n", rows, buffer[:rows])
 }
