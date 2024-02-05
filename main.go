a4c306412fbb2b56ee5ba22a70b09c99a94d4b2f
Dean Wan <deanwan14@gmail.com>
2024-02-05 12:46:46 +1100

    feat: populate question logic

--- main.go
+++ main.go
@@ -9,6 +9,8 @@ import (
 )
 
 func main() {
+	var correctAnswers int
+
 	// Parse command line flags
 	fileNamePtr := flag.String("filename", "./problems.csv", "file location")
 	flag.Parse()
@@ -24,8 +26,26 @@ func main() {
 	csvReader := csv.NewReader(file)
 	questions, err := csvReader.ReadAll()
 	if err != nil {
+		// Will exit if cell lengths are not all the same
 		log.Fatal(err)
 	}
 
-	fmt.Println(questions)
+	totalQuestions := len(questions)
+
+	// Iterate through questions
+	for _, question := range questions {
+		// Ask questions
+		fmt.Println(question[0])
+		var answer string
+		fmt.Scanln(&answer)
+
+		if answer == question[1] {
+			fmt.Println("Correct!")
+			correctAnswers++
+		} else {
+			fmt.Println("Whoops, that's not correct!")
+		}
+	}
+
+	fmt.Printf("You got %d out of %d questions correct! Thanks for playing!\n", correctAnswers, totalQuestions)
 }
